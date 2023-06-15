1422458d830b44c35e9c0e56e775980a5d11d564
Aswathy1638 <aswathy1638@gmail.com>
2023-06-15 14:10:50 +0530

    commit

--- week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
+++ week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
@@ -18,8 +18,41 @@
             //2. Get all people above 30
             //3. Sort people by name
             //4. Project/Select only Name and Country of all people
+            var personlinq = from p in people
+                              where p.Country == "USA"
+                              select p;
+            Console.WriteLine("People from USA");
+
+            foreach (var person in personlinq)
+            {
+                Console.WriteLine(person.Name);
+            }
+            Console.WriteLine("People above 20");
+
+            var personlinq1 = from p in people
+                              where p.Age > 20
+                              select p;
+            foreach (var person in personlinq1)
+            {
+                Console.WriteLine(person.Name);
+            }
+            var personlinq2 = people.OrderBy(p => p.Name);
+            Console.WriteLine("Sorted  name");
+            foreach (var person in personlinq2)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            var personlinq3 = people.Select(p => new {p.Name,p.Country});
+
+            foreach (var person in personlinq3)
+            {
+                Console.WriteLine($"{ person.Name} lives in { person.Country}");
+            }
 
         }
+
+
     }
 
     public class Person
