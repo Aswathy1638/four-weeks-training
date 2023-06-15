964e0d925fb4d0d17bcba15821597dc881f2f74e
Aswathy1638 <aswathy1638@gmail.com>
2023-06-15 17:02:00 +0530

    commit

--- week-1/day-4/exercise-8/LinqGroupAggregate/Program.cs
+++ week-1/day-4/exercise-8/LinqGroupAggregate/Program.cs
@@ -17,9 +17,40 @@
 
             // Use LINQ to perform the following operations:
             // 1. Group products by category
+
+
+            var groupByCategory= from pdt in products
+                                 group pdt by pdt.Category into catGrp
+                                 select catGrp;
             // 2. Count the number of products in each category
+            var productCountsByCategory = from category in groupByCategory
+                                          select new { Category = category.Key, Count = category.Count() };
+
+            Console.WriteLine("List of products in each Category");
+            foreach (var count in productCountsByCategory)
+            {
+                Console.WriteLine("{0}: {1}", count.Category, count.Count);
+            }
             // 3. Calculate the total price of products in each category
+            var totalPriceByCategory = from category in groupByCategory
+                                       select new { Category = category.Key, TotalPrice = category.Sum(product => product.Price) };
+
+            Console.WriteLine("Total price of products by category:");
+            foreach (var totalPrice in totalPriceByCategory)
+            {
+                Console.WriteLine("{0}: {1}", totalPrice.Category, totalPrice.TotalPrice);
+            }
+
             // 4. Find the most expensive product in each category
+            var mostExpensiveByCategory = from category in groupByCategory
+                                          select new { Category = category.Key, MostExpensive = category.OrderByDescending(product => product.Price).FirstOrDefault() };
+
+            Console.WriteLine("Most expensive products by category:");
+            foreach (var mostExpensive in mostExpensiveByCategory)
+            {
+                Console.WriteLine("{0}: {1} ({2:C})", mostExpensive.Category, mostExpensive.MostExpensive.Name, mostExpensive.MostExpensive.Price);
+            }
+
         }
     }
 }
