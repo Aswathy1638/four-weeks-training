66d8315f259e8437a60b0ddcd263e23e005e4fd1
Aswathy1638 <aswathy1638@gmail.com>
2023-06-15 11:06:03 +0530

    commit

--- week-1/day-4/exercise-1/StackApp/ICustomStack.cs
+++ week-1/day-4/exercise-1/StackApp/ICustomStack.cs
@@ -5,5 +5,37 @@
         void Push(T item);
         T Pop();
         bool IsEmpty();
+
+
+    }
+    public class Stack <T>: ICustomStack<T>
+    {
+        private class Node
+        {
+            public T Data { get; set; }
+            public Node Next { get; set; }
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+        private Node top;
+        public void Push(T item)
+        {
+            Node newNode = new Node(item);
+            newNode.Next = top;
+            top = newNode;
+        }
+        public T Pop()
+        {
+            T data = top.Data;
+            top = top.Next;
+            return data;
+        }
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
     }
 }
--- week-1/day-4/exercise-1/StackApp/Program.cs
+++ week-1/day-4/exercise-1/StackApp/Program.cs
@@ -4,13 +4,30 @@
     {
         static void Main(string[] args)
         {
-            //ICustomStack<int> intStack = new CustomStack<int>();
-            //intStack.Push(1);
-            //intStack.Push(2);
-            //intStack.Push(3);
-            //Console.WriteLine(intStack.Pop()); // Output: 3
-            //Console.WriteLine(intStack.Pop()); // Output: 2
-            //Console.WriteLine(intStack.IsEmpty()); // Output: False
+
+            Stack<int> intStack = new Stack<int>();
+            intStack.Push(1);
+            intStack.Push(2);
+            intStack.Push(3);
+            Console.WriteLine(intStack.Pop()); // Output: 3
+            Console.WriteLine(intStack.Pop()); // Output: 2
+            Console.WriteLine(intStack.IsEmpty()); // Output: False
+            
+           Stack<float> floatStack = new Stack<float>();
+            floatStack.Push(8.8f);
+            floatStack.Push(2.3f);
+            Console.WriteLine(floatStack.Pop());
+            Console.WriteLine(floatStack.Pop());
+            Console.WriteLine(floatStack.IsEmpty());
+
+            Stack<string> stringStack = new Stack<string>();
+            stringStack.Push("Hello");
+            stringStack.Push("26 ");
+            Console.WriteLine(stringStack.Pop());
+            Console.WriteLine(stringStack.Pop());
+            Console.WriteLine(stringStack.IsEmpty());
+
+
         }
     }
 }
