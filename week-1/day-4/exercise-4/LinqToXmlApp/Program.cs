b588db99b3cc0130ce34278618701cbd8800b84e
Aswathy1638 <aswathy1638@gmail.com>
2023-06-15 14:47:19 +0530

    commit

--- week-1/day-4/exercise-4/LinqToXmlApp/Program.cs
+++ week-1/day-4/exercise-4/LinqToXmlApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace LinqToXmlApp
+﻿using System.Xml.Linq;
+
+namespace LinqToXmlApp
 {
     internal class Program
     {
@@ -16,27 +18,39 @@
             // Write above book structure as a c# string
             string xmlString = @"<Books>
                                     <Book>
-                                        <Title>Book Title 1</Title>
-                                        <Author>Author 1</Author>
-                                        <Genre>Genre 1</Genre>
+                                        <Title>A passage to India </Title>
+                                        <Author>E M Foster</Author>
+                                        <Genre>Travel </Genre>
                                     </Book>
                                     <Book>
-                                        <Title>Book Title 2</Title>
-                                        <Author>Author 2</Author>
-                                        <Genre>Genre 2</Genre>
+                                        <Title>Harry potter</Title>
+                                        <Author>J K R </Author>
+                                        <Genre> fantacy</Genre>
                                     </Book>
                                     <Book>
-                                        <Title>Book Title 3</Title>
-                                        <Author>Author 3</Author>
-                                        <Genre>Genre 3</Genre>
+                                        <Title>Making India awesome </Title>
+                                        <Author>Chetan Bhagath </Author>
+                                        <Genre>Political</Genre>
                                     </Book>
                                 </Books>";
 
-            // Create an XDocument object from the XML string
+            // Create an XDocument object from the XML
+
+            XDocument doc = XDocument.Parse(xmlString);
+            Console.WriteLine(doc);
+
 
             // Write the title of all books to the console
+            foreach (var book in doc.Descendants("Book"))
+            {
+                Console.WriteLine(book.Element("Title").Value);
+            }
 
             // Write the title of all books with genre "Genre 1" to the console
+            foreach (var book in doc.Descendants("Book").Where(b => b.Element("Genre").Value == "Political"))
+            {
+                Console.WriteLine(book.Element("Title").Value);
+            }
 
         }
     }
