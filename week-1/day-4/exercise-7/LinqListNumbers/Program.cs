44343646d3a4c2f430d6d4e8080e54b2ce6e65ba
Aswathy1638 <aswathy1638@gmail.com>
2023-06-15 16:01:34 +0530

    commit

--- week-1/day-4/exercise-7/LinqListNumbers/Program.cs
+++ week-1/day-4/exercise-7/LinqListNumbers/Program.cs
@@ -9,10 +9,40 @@
 
             // Use LINQ to perform the following operations:
             // 1. Find all even numbers
+            var evenLinq = from x in numbers
+                           where x%2 == 0
+                           select x;
+            Console.WriteLine("EVEN NUMBERS ARE:");
+            foreach (var x in evenLinq)
+            {
+                Console.WriteLine(x);
+            }
             // 2. Find all numbers greater than a specific value (e.g., 20)
+            var GreatLinq = from x in numbers
+                            where x > 20
+                            select x;
+            Console.WriteLine("NUMBERS HIGHER THAN 20 ARE:");
+            foreach (var x in GreatLinq)
+            {
+                Console.WriteLine(x);
+            }
             // 3. Calculate the sum of all numbers
+            var SumLinq = from x in numbers
+                          select x;
+            int sum = SumLinq.Sum();
+            Console.WriteLine($"{sum} is the sum");
+
             // 4. Calculate the average of all numbers
+            var AvgLinq = from x in numbers
+                          select x;
+            double avg = AvgLinq.Average();
+            Console.WriteLine($"{avg} is the average");
             // 5. Find the minimum and maximum values in the list
+            var MinMaxLinq= from x in numbers
+                             select x;
+            int min = MinMaxLinq.Min();
+            int max = MinMaxLinq.Max();
+            Console.WriteLine($"{min} is the minimum value and {max} is the maximum value");
         }
     }
 }
