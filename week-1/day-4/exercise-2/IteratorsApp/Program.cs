9392ba495fde131f4f31498b7e8f0d3d448adcef
Aswathy1638 <aswathy1638@gmail.com>
2023-06-15 12:48:14 +0530

    commit

--- week-1/day-4/exercise-2/IteratorsApp/Program.cs
+++ week-1/day-4/exercise-2/IteratorsApp/Program.cs
@@ -1,20 +1,53 @@
-﻿namespace IteratorsApp
+﻿using System.Collections;
+using System.Runtime.ExceptionServices;
+
+namespace IteratorsApp
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            var fibonacci = FibonacciSequence().Take(10);
-            foreach (int number in fibonacci)
+        
+            FibonacciSequence fib = new FibonacciSequence(10);
+
+            foreach (int number in fib)
             {
                 Console.WriteLine(number);
             }
         }
+
+
+
+
         // https://www.c-sharpcorner.com/UploadFile/5ef30d/understanding-yield-return-in-C-Sharp/
         // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/yield
-        public static IEnumerable<int> FibonacciSequence()
+
+        public class FibonacciSequence : IEnumerable
         {
-            throw new NotImplementedException();
+            int count;
+            public FibonacciSequence(int count)
+            {
+                this.count = count;
+            }
+            public IEnumerator GetEnumerator()
+            {// throw new NotImplementedException(); 
+
+
+                int first = 0;
+                int next = 1;
+
+
+                for (int i = 0; i < count; i++)
+                {
+                    yield return first;
+                    int temp = first;
+                    first = next;
+                    next = temp + b;
+                }
+
+            }
+
         }
     }
 }
