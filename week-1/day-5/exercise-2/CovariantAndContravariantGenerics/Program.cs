0e89f5eff5f18dcde40510f85c3346b7ed266756
Aswathy1638 <aswathy1638@gmail.com>
2023-06-16 14:21:14 +0530

    commit all

--- week-1/day-5/exercise-2/CovariantAndContravariantGenerics/Program.cs
+++ week-1/day-5/exercise-2/CovariantAndContravariantGenerics/Program.cs
@@ -10,7 +10,8 @@
         // Implement Process method
         public int Process(string input)
         {
-            throw new NotImplementedException();
+            int legth = input.Length;
+            return legth;
         }
     }
 
@@ -19,7 +20,8 @@
         // Implement Process method
         public string Process(double input)
         {
-            throw new NotImplementedException();
+           string str = input.ToString();
+            return str;
         }
     }
     internal class Program
@@ -27,6 +29,21 @@
         static void Main(string[] args)
         {
             // Demonstrate covariance and contravariance with IProcessor interface
+            IProcessor<string,int> processor = new StringToIntProcessor();
+            string str = "This is Covariance";
+            object obj = processor.Process(str);
+            Console.WriteLine("Covariance");
+            Console.WriteLine($"Length of input string is {obj}");
+
+            IProcessor<double,string> processor1 = new DoubleToStringProcessor();
+            double inputDouble = 3.14;
+            Console.WriteLine("Cotravarient");
+            string sresult=processor1.Process(inputDouble);
+            Console.WriteLine($"The string representation is{sresult}");
+
+
+
         }
+       
     }
 }
